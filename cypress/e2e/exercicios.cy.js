8748bde5700b205f09a95a42f9c93e70c056d31a
carolainesantos <carolainedossants@gmail.com>
2026-05-02 19:03:40 -0300

    fix: exercises

--- cypress/e2e/exercicios.cy.js
+++ cypress/e2e/exercicios.cy.js
@@ -1,9 +1,16 @@
-describe("Exercicios de fixação", () => {
-  it("passes", () => {
+describe("Lista de exercicios", () => {
+  it("Exercício 1: Visitar a página do AdoPet e clicar no botão 'Ver pets disponíveis para adoção'", () => {
     cy.visit("https://adopet-frontend-cypress.vercel.app/");
     cy.contains("a", "Ver pets disponíveis para adoção").click();
-    cy.get("a.header__home").click();
-    // cy.get("a.header__message").click();
-    cy.contains("a", "Fazer login").click();
+  });
+
+  it("Exercício 2: Visitar a página do AdoPet e testar os botoes header", () => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app/");
+    cy.get('[data-cy="home-link"]').click();
+  });
+
+  it("Exercício 3: Visitar a página de Login do AdoPet", () => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app/login");
+    cy.get("a.header__message").click();
   });
 });
