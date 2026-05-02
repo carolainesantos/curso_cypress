e42209aff5d5c42a58eb7a691a802c229c154ef4
carolainesantos <carolainedossants@gmail.com>
2026-05-02 20:45:26 -0300

    feat: mochawesome and video

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/results",
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss",
+    },
   },
 });
